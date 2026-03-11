f068926c9df372e37b1ca46440c3a3a001249bca
anniewho99 <xinyuehu1999@gmail.com>
2026-03-11 08:52:05 -0700

    new condition

--- examples/groupestimation/groupestimation.js
+++ examples/groupestimation/groupestimation.js
@@ -1088,7 +1088,7 @@ let assigendCondition;
 //   return allowed.has(cond) ? cond : null;
 // }
 
-assigendCondition ='cdab';
+assigendCondition ='bcda';
 
 console.log('assigned condition is', assigendCondition);
 
@@ -1423,7 +1423,7 @@ const dabc = {
 
 // applyCondition(forced);
 
-let levelPlacements = cdab;
+let levelPlacements = bcda;
 //cdab
 // const levelPlacements = {
 //   0: {
